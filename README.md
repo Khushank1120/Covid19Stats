592e15ea93856c840d210226691b05a0581871d2
Khushank <mistry.khushank@gmail.com>
2020-08-16 22:21:27 +0530

    Initial commit-1

---
+++ README.md
@@ -0,0 +1,12 @@
+# Covid19Statistics
+
+## General Info
+
+**An android app that shows Coronavirus cases, recovered, deaths, active cases, cases tested positive today, deaths happened today, affected countries and various other stats all over the world.**
+
+## Technologies Used
+
+* Android Studio version-3.6.3
+* Used Volley Library used to transmit data.
+* Used android plugin EasyGraph for the Pie Chart and SimpleArcLoader for the loading animation.
+* Also used REST API https://corona.lmao.ninja/v2/all for statistics of cases, recovered, deaths, active cases, cases tested positive today, deaths happened today, affected countries and various other and https://corona.lmao.ninja/v2/country for statistics of all the countires.
---
+++ app/src/main/java/myapplication2/example/covid_19stats/DetailsActivity.java
@@ -0,0 +1,57 @@
+package myapplication2.example.covid_19stats;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private int positionCountry;
+    TextView tvCountry,tvCases,tvRecovered,tvCritical,tvActive,tvCasesToday,tvTotalDeaths,tvDeathsToday;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Intent intent = getIntent();
+        positionCountry = intent.getIntExtra("position",0);
+
+        getSupportActionBar().setTitle("Details of "+AffectedCountries.countryModelsList.get(positionCountry).getCountry());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+
+        tvCountry = findViewById(R.id.tvCountry);
+        tvCases = findViewById(R.id.tvCases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvCasesToday = findViewById(R.id.tvCasesToday);
+        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        tvDeathsToday = findViewById(R.id.tvDeathsToday);
+
+        tvCountry.setText(AffectedCountries.countryModelsList.get(positionCountry).getCountry());
+        tvCases.setText(AffectedCountries.countryModelsList.get(positionCountry).getCases());
+        tvRecovered.setText(AffectedCountries.countryModelsList.get(positionCountry).getRecovered());
+        tvCritical.setText(AffectedCountries.countryModelsList.get(positionCountry).getCritical());
+        tvActive.setText(AffectedCountries.countryModelsList.get(positionCountry).getActive());
+        tvCasesToday.setText(AffectedCountries.countryModelsList.get(positionCountry).getTodayCases());
+        tvTotalDeaths.setText(AffectedCountries.countryModelsList.get(positionCountry).getDeaths());
+        tvDeathsToday.setText(AffectedCountries.countryModelsList.get(positionCountry).getTodayDeaths());
+
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home)
+            finish();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
